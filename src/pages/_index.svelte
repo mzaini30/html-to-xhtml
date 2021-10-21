f101fbb76fc4f946ae60e48faf7c7baebf335d49
mzaini30 <muhzaini30@gmail.com>
2021-10-21 14:47:34 +0800

    oke

--- src/pages/_index.svelte
+++ src/pages/_index.svelte
@@ -1,4 +1,17 @@
 <script>
 	let input
 	let output
+
+	const browser = !import.meta.env.SSR
+
+	$: if (input){
+		if (browser) {
+			let olah = new DOMParser().parseFromString(input, 'text/html')
+			olah = new XMLSerializer().serializeToString(olah)
+			output = olah
+		}
+	}
 </script>
+
+<textarea bind:value={input} placeholder="Tulis kodenya di sini"></textarea>
+<textarea bind:value={output} readonly></textarea>
